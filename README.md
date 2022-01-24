4bb78c5a11e9e861fc0b383647d4dbce0b2486aa
jjmilano <jjmilanof1@gmail.com>
2022-01-24 19:47:56 -0300

    segundo taller on progress

--- README.md
+++ README.md
@@ -7,3 +7,12 @@
 - Segundo paso : Implementar las formulas en JavaScript
 - Tercer paso : Crear funciones y probarlas.
 - Cuarto paso : Integrar Js con HTML Y que funcione.
+
+
+
+## Taller nº2 : Calculadora de porcentajes y descuentos.
+
+- primer paso : definir las formulas para su ejecusión
+- segundo paso : implementar las formulas en javaScript
+- tercer paso: crear funciones y probarlas.
+- Cuarto paso: Integrar el Javascript con el HTML
--- Talleres/porcentajes.js
+++ Talleres/porcentajes.js
@@ -1,3 +1,50 @@
 function Descuentos(precio,descuento){
-    return (precio *(100-descuento)/100);
+const porcentajePrecioConDescuento = 100 - descuento;
+const precioConDescuento = (precio*porcentajePrecioConDescuento)/100
+
+return precioConDescuento;
+}
+
+// function descuentoMagico(){
+//     const inputPrice = document.getElementById("InputPrice");
+//     const priceValue = inputPrice.value;
+
+//     const inputDiscount = document.getElementById("InputDiscount");
+//     const discountValue = inputDiscount.value;
+
+//     const precioConDescuento = Descuentos(priceValue,discountValue);
+
+//     const resultP = document.getElementById("resultP");
+//     resultP.innerText = "El precio con descuento son : $" + precioConDescuento;
+// }
+
+
+function descuentoMagico(){
+    const coupons =[
+        "Cupon123",
+        "Cupon234",
+        "Cupon567",
+    ];
+    const inputPrice = document.getElementById("InputPrice");
+    const priceValue = inputPrice.value;
+
+    const inputCoupon = document.getElementById("InputCoupon");
+    const counponValue = inputCoupon.value;
+   
+        let descuento;
+        switch(counponValue){
+        case coupons[0]:
+            descuento = 15;
+        break;
+        case coupons[1]:
+            descuento = 30;
+        break;
+        case coupons[2]:
+            descuento =25;
+        break;
+        }
+
+    const precioConDescuento = Descuentos(priceValue,descuento);
+    const resultP= document.getElementById("resultP");
+    resultP.innerText = "el precio con descuento son $" + precioConDescuento;
 }
